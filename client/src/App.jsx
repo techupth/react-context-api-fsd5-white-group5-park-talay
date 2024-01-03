df115b31da1d429a06a8e40c8874cdf84aec89d5
Pacharadanai <pacharadanaipinake@gmail.com>
2024-01-03 12:54:31 +0700

    feat: add completed exercise

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,8 @@ import "./App.css";
 import HomePage from "./pages/HomePage.jsx";
 import ViewProductPage from "./pages/ViewProductPage.jsx";
 
+import { UserDataContext } from "./contexts/UserDataContext.jsx";
+
 function App() {
   const userData = {
     username: "John",
@@ -12,14 +14,16 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/product/view/:id" element={<ViewProductPage />} />
-        </Routes>
-      </Router>
-    </div>
+    <UserDataContext.Provider value={{ userData }}>
+      <div className="App">
+        <Router>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/product/view/:id" element={<ViewProductPage />} />
+          </Routes>
+        </Router>
+      </div>
+    </UserDataContext.Provider>
   );
 }
 
